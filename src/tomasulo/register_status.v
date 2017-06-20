ff66602c7c7aff8d7381c8bfdde9b5591779f6bd
Ahmed Emad Barakat <Ahmed.E.Barakat1994@gmail.com>
2017-06-20 08:27:27 +0200

    Fix: write the tag and val correctly in registers

--- src/tomasulo/register_status.v
+++ src/tomasulo/register_status.v
@@ -22,15 +22,19 @@ module REG_STATUS
 );
 
 parameter INVALID_TAG = 5'b11111;
-integer i;
 
 reg[36:0] registers[0:31]; // 36:5 val , 4:0 tag
 
+integer i_0;
 initial begin
   out_enable = 1'b0;
-  for (i = 0;i < 32; i=i+1) begin
-    registers[i] = {32'bx, INVALID_TAG};
+  for (i_0 = 0; i_0 < 32; i_0=i_0+1) begin
+    registers[i_0] = {32'bx, INVALID_TAG};
   end
+  registers[0] = {32'h1, INVALID_TAG};
+  registers[1] = {32'h3, INVALID_TAG};
+  registers[3] = {32'h6, INVALID_TAG};
+  registers[4] = {32'h7, INVALID_TAG};
 end
 
 always @(posedge in_bank_enable) begin
@@ -38,23 +42,26 @@ always @(posedge in_bank_enable) begin
     registers[in_bank_reg][36:5] = 32'bx;
 end
 
+integer i_1;
 always @(posedge in_CDB_broadcast) begin
-  for (i = 0;i < 31; i = i+1) begin
-    if (registers[i][4:0] == in_CDB_tag) begin
-      registers[i][36:5] = in_CDB_val;
-      registers[i][4:0] =  INVALID_TAG;
+  $display("At time %4t, write back tag: %4d with value: %4d\n", $time, in_CDB_tag, in_CDB_val);
+  for (i_1 = 0; i_1 < 31; i_1=i_1+1) begin
+    if (registers[i_1][4:0] == in_CDB_tag) begin
+      registers[i_1][36:5] = in_CDB_val;
+      registers[i_1][4:0] =  INVALID_TAG;
     end
   end
 end
 
 always @(posedge in_enable) begin
-  out_val_1 <= registers[in_reg_1][36:5];
-  out_val_2 <= registers[in_reg_2][36:5];
+  #5;
+  out_val_1 = registers[in_reg_1][36:5];
+  out_val_2 = registers[in_reg_2][36:5];
 
   // TODO: Write back or reorder buffer should proceed from here.
 
-  out_tag_1 <= registers[in_reg_1][4:0];
-  out_tag_2 <= registers[in_reg_2][4:0];
+  out_tag_1 = registers[in_reg_1][4:0];
+  out_tag_2 = registers[in_reg_2][4:0];
   out_enable = 1'b1;
   #5;
   out_enable = 1'b0;
--- src/tomasulo/register_status_tb.v
+++ src/tomasulo/register_status_tb.v
@@ -1,79 +1,81 @@
+`include "register_status.v"
 module test;
 
-  reg clk = 0;
-  always #5 clk = !clk;
+reg clk = 0;
+always #5 clk = !clk;
 
-  reg [4:0] in_reg_1;
-  reg [4:0] in_reg_2;
+reg [4:0] in_reg_1;
+reg [4:0] in_reg_2;
 
-  reg in_bank_enable;
-  reg [4:0] in_bank_reg;
-  reg [4:0] in_bank_tag;
+reg in_bank_enable;
+reg [4:0] in_bank_reg;
+reg [4:0] in_bank_tag;
 
-  reg in_CDB_broadcast;
-  reg [4:0] in_CDB_tag;
-  reg [31:0] in_CDB_val;
+reg in_CDB_broadcast;
+reg [4:0] in_CDB_tag;
+reg [31:0] in_CDB_val;
 
-  wire out_enable;
-  wire [31:0] out_val_1;
-  wire [31:0] out_val_2;
-  wire [4:0] out_tag_1;
-  wire [4:0] out_tag_2;
+wire out_enable;
+wire [31:0] out_val_1;
+wire [31:0] out_val_2;
+wire [4:0] out_tag_1;
+wire [4:0] out_tag_2;
 
-  REG_STATUS reg_status (
-                         clk,
+REG_STATUS reg_status (
+                       clk,
 
-                         clk,
-                         in_reg_1,
-                         in_reg_2,
+                       clk,
+                       in_reg_1,
+                       in_reg_2,
 
-                         in_bank_enable,
-                         in_bank_reg,
-                         in_bank_tag,
+                       in_bank_enable,
+                       in_bank_reg,
+                       in_bank_tag,
 
-                         in_CDB_broadcast,
-                         in_CDB_tag,
-                         in_CDB_val,
+                       in_CDB_broadcast,
+                       in_CDB_tag,
+                       in_CDB_val,
 
-                         out_enable,
-                         out_val_1,
-                         out_val_2,
-                         out_tag_1,
-                         out_tag_2
-                         );
+                       out_enable,
+                       out_val_1,
+                       out_val_2,
+                       out_tag_1,
+                       out_tag_2
+                       );
 
-  initial begin
-    in_bank_enable = 1'b0;
-    in_CDB_broadcast = 1'b0;
+initial begin
+  in_bank_enable = 1'b0;
+  in_CDB_broadcast = 1'b0;
 
-    #5;
-    in_reg_1 = 5'b101;
+  #5;
+  in_reg_1 = 5'b101;
 
-    #5;
-    in_bank_reg = 5'b101;
-    in_bank_tag = 5'b1;
-    in_bank_enable = 1'b1;
+  #5;
+  in_bank_reg = 5'b101;
+  in_bank_tag = 5'b1;
+  in_bank_enable = 1'b1;
 
-    #5;
-    in_CDB_tag = 5'b1;
-    in_CDB_val = 32'b111;
-    in_CDB_broadcast = 1'b1;
+  #5;
+  in_CDB_tag = 5'b1;
+  in_CDB_val = 32'b111;
+  in_CDB_broadcast = 1'b1;
 
-    #5;
-    in_reg_2 = 5'b1;
-    #10 $finish;
-  end
+  #5;
+  in_reg_2 = 5'b1;
+  #10 $finish;
+end
 
-  initial
-     $monitor(
-              "At time %2t, ", $time,
-              "in_reg_1: %2d, ", in_reg_1,
-              "in_reg_2: %2d, ", in_reg_2,
-              "out_val_1: %d, ", out_val_1,
-              "out_val_2: %d, ", out_val_2,
-              "out_tag_1: %2d, ", out_tag_1,
-              "out_tag_2: %2d\n", out_tag_2,
-              "out_enable: %2d\n", out_enable
-              );
+initial
+   $monitor(
+            "At time %4t\n", $time,
+            "in_reg_1: %4d\n", in_reg_1,
+            "in_reg_2: %4d\n", in_reg_2,
+            "out_val_1: %4d\n", out_val_1,
+            "out_val_2: %4d\n", out_val_2,
+            "out_tag_1: %4d\n", out_tag_1,
+            "out_tag_2: %4d\n", out_tag_2,
+            "out_enable: %4d\n", out_enable,
+            "\n--------------------\n"
+            );
 
 endmodule
